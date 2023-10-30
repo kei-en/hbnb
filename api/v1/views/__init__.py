6a01f909ce0e24a6eaefaddbec95920c243029da
kei-en <njuguna0karanja@gmail.com>
2023-10-31 02:34:05 +0300

    Reviews, Place - Amenity

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,3 +6,5 @@ from api.v1.views.cities import *  # noqa
 from api.v1.views.amenities import *  # noqa
 from api.v1.views.users import *  # noqa
 from api.v1.views.places import *  # noqa
+from api.v1.views.places_reviews import *  # noqa
+from api.v1.views.places_amenities import *  # noqa
